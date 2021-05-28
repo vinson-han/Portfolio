38fec884b4db00853743cf804ad4a958c2cdf4f9
vinson-han <Han_vinson@student.smc.edu>
2021-05-27 23:38:37 -0700

    adding cards

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 
 let i =0,text
-text = "Full Stack Web Developer."
+text = "I'm a Full Stack Web Developer."
 
 
 function typing(){
 
     if(i < text.length){
         document.getElementById("roles").innerHTML += text.charAt(i)
-        console.log(text.charAt(i))
         i++;
         setTimeout(typing,50);
     }
 }
 
 typing();
+
+
+// function redeemShoot() {
+//     return new Promise((resolve) =>  {
+//       document.querySelector('[aria-label="Points Balance"]').click();
+//       setTimeout(() => {
+//         document.querySelector('#channel-points-reward-center-body button').click();
+//         setTimeout(() => {
+//           document.querySelector('#channel-points-reward-center-body button').click();
+//           setTimeout(() => {
+//  g
+//           }, 700);
+//         }, 300)
+//       }, 300);
+//     });
+//     }
+//     promise = redeemShoot()
+//     for (i = 0; i < 100; i += 1) { promise = promise.then(() => redeemShoot()) }
